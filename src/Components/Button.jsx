2e929e8db327536a5c2fdf3607deb7201ee9c168
görkem serin <gorkemserin@outlook.com>
2023-11-12 13:57:08 +0300

    Special Offer Section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,14 +1,25 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className=" flex justify-center items-center gap-2 px-7 py-4 border rounded-full font-montserrat text-lg leading-none bg-coral-red  text-white">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 
+      py-4 border  font-montserrat text-lg 
+      leading-none rounded-full ${backgroundColor ? "${backgroundColor} ${textColor} ${borderColor}" : "bg-coral-red  text-white" } `}
+    >
       {label}
 
-      {iconURL &&<img
-        src={iconURL}
-        alt="arrow icon"
-        className="ml-2 rounded-full h-5 aspect-square"
-      />
-      }
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow icon"
+          className="ml-2 rounded-full h-5 aspect-square"
+        />
+      )}
     </button>
   );
 };
