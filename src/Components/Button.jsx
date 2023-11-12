20c0a52c990c1cf47b0f335592567262fbb3ffc5
görkem serin <gorkemserin@outlook.com>
2023-11-12 13:27:34 +0300

    Super Quality Section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,11 +3,12 @@ const Button = ({ label, iconURL }) => {
     <button className=" flex justify-center items-center gap-2 px-7 py-4 border rounded-full font-montserrat text-lg leading-none bg-coral-red  text-white">
       {label}
 
-      <img
+      {iconURL &&<img
         src={iconURL}
         alt="arrow icon"
         className="ml-2 rounded-full h-5 aspect-square"
       />
+      }
     </button>
   );
 };
